87b18b93cfcf4fe41eb0bc072aef2913d16da99a
Aaron Schreck <schreck.aaron@gmail.com>
2019-10-15 20:46:59 -0600

    temporarily remove module and enable redirection.

---
+++ urlshort-master/main/handler.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	//	TODO: Implement this...
+
+	// so the map handler is going to either be one that actually does something, or one that returns hello world.
+	return func(w http.ResponseWriter, r *http.Request) {
+		// so this function is going to execute and needs to send back a function capable of executing and doing what is needed.
+		returnedURL := ""
+		for registeredURL := range pathsToUrls {
+			incomingURL := r.URL.String()
+			fmt.Println(incomingURL)
+			fmt.Println(registeredURL)
+			if incomingURL == registeredURL {
+				returnedURL = pathsToUrls[registeredURL]
+				fmt.Println(returnedURL)
+			}
+		}
+		if !(returnedURL == "") {
+			// w.WriteHeader(302)
+			// w.Header().Set("Location", returnedURL)
+			http.Redirect(w, r, returnedURL, 302)
+		}
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// TODO: Implement this...
+	return nil, nil
+}
